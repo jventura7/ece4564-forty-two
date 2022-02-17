96842dbf459deb139351b6a84d7604a343593878
KurubellG <60444770+KurubellG@users.noreply.github.com>
2022-02-17 00:38:05 -0500

    Changes to get keys imported and get first tweet #

--- main - Copy.py
+++ main - Copy.py
@@ -1,10 +1,12 @@
 #Code from youtube source to extract Twitter posts
+from clientKeys import *
 import tweepy
-auth = tweepy.OAuthHandler("Omiemks7GHNo9QO3WuNZxrbaW", "cCChIK5zIFOT1qDJ1ZjjmyQLh565yTveIeHBCQBrrMEwQCTlmA")
-auth.set_access_token("1494032499625840653-PfkRAdSxA05vUZdklE0SIOGm0nGBst", "d7VXRNRvOsUWwweQTXDGYUebRaxIrEHMJ9FNHweFYQyIj")
+auth = tweepy.OAuthHandler(ckey, csecret)
+auth.set_access_token(atoken, asecret)
 
 api = tweepy.API(auth)
 public_tweets = api.home_timeline()
 
 for tweet in public_tweets:
-    print(tweet.text)
+    if "#ECE4564T14" in tweet.text:
+        print(tweet.text)
