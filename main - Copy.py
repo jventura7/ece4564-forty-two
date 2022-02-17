3543e7e82ae8fee924ba9ee9cb2df6569536fbf9
KurubellG <60444770+KurubellG@users.noreply.github.com>
2022-02-17 13:40:18 -0500

    twitter question + wolfram response + server keys

--- main - Copy.py
+++ main - Copy.py
@@ -1,12 +1,32 @@
-#Code from youtube source to extract Twitter posts
+# Code from youtube source to extract Twitter posts
 from clientKeys import *
+from serverKeys import *
 import tweepy
+import wolframalpha
+
 auth = tweepy.OAuthHandler(ckey, csecret)
 auth.set_access_token(atoken, asecret)
 
 api = tweepy.API(auth)
 public_tweets = api.home_timeline()
-
+tweetStr = ""
+hashtag = "#ECE4564T14"
 for tweet in public_tweets:
-    if "#ECE4564T14" in tweet.text:
-        print(tweet.text)
+    if hashtag in tweet.text:
+        tweetStr = tweet.text
+        break
+
+question = tweetStr.replace(" " + hashtag, "", 1)
+
+print("Question:", question)
+# Find a response to the question
+wolfclient = wolframalpha.Client(wolframID)
+print("Question sent, waiting for response...")
+result = wolfclient.query(question)
+try:
+    txtAnswer = next(result.results).text
+    print("Answer:", txtAnswer)
+except StopIteration:
+    print("Error: Invalid Question, WolframAlpha cannot answer")
+
+
