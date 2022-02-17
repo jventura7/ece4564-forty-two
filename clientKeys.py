5637e222b80b97d089736ae0095f73f55fb336c3
noahsanzone <60456771+noahsanzone@users.noreply.github.com>
2022-02-17 13:54:21 -0500

    Delete clientKeys.py

--- clientKeys.py
+++
@@ -1,4 +0,0 @@
-ckey = "Omiemks7GHNo9QO3WuNZxrbaW"
-csecret = "cCChIK5zIFOT1qDJ1ZjjmyQLh565yTveIeHBCQBrrMEwQCTlmA"
-atoken = "1494032499625840653-PfkRAdSxA05vUZdklE0SIOGm0nGBst"
-asecret = "d7VXRNRvOsUWwweQTXDGYUebRaxIrEHMJ9FNHweFYQyIj"
