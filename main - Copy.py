820d5b786f5427a6900fae14ccdd4718bc8b8027
KurubellG <60444770+KurubellG@users.noreply.github.com>
2022-02-20 18:35:17 -0500

    Continuous stream

--- main - Copy.py
+++ main - Copy.py
@@ -6,29 +6,39 @@ import wolframalpha
 
 auth = tweepy.OAuthHandler(ckey, csecret)
 auth.set_access_token(atoken, asecret)
+keepLooping = True
+while keepLooping:
+    keepGoing = input('Look for tweets? Y/N: ')
+    if keepGoing == 'Y':
+        api = tweepy.API(auth)
+        public_tweets = api.home_timeline()
+        tweetStr = ""
+        hashtag = "#ECE4564T14"
+        for tweet in public_tweets:
+            if hashtag in tweet.text:
+                tweetStr = tweet.text
+                break
 
-api = tweepy.API(auth)
-public_tweets = api.home_timeline()
-tweetStr = ""
-hashtag = "#ECE4564T14"
-for tweet in public_tweets:
-    if hashtag in tweet.text:
-        tweetStr = tweet.text
-        break
-
-question = tweetStr.replace(hashtag, "")
-question = question.lstrip()
-question = question.rstrip()
-if len(question) == 0:
-    print("Error: No question asked retry with question + hashtag")
-else:
-    print("Question:", question)
-    # Find a response to the question
-    wolfclient = wolframalpha.Client(wolframID)
-    print("Question sent, waiting for response...")
-    result = wolfclient.query(question)
-    try:
-        txtAnswer = next(result.results).text
-        print("Answer:", txtAnswer)
-    except StopIteration:
-        print("Error: Invalid Question, WolframAlpha cannot answer")
+        question = tweetStr.replace(hashtag, "")
+        question = question.lstrip()
+        question = question.rstrip()
+        if len(question) == 0:
+            print("Error: No question asked retry with question + hashtag")
+        else:
+            print("Question:", question)
+            # Find a response to the question
+            wolfclient = wolframalpha.Client(wolframID)
+            print("Question sent, waiting for response...")
+            result = wolfclient.query(question)
+            try:
+                txtAnswer = next(result.results).text
+                print("Answer:", txtAnswer)
+            except StopIteration:
+                print("Error: Invalid Question, WolframAlpha cannot answer")
+        keepLooping = True
+    elif keepGoing == 'N':
+        print("Stopped program.")
+        keepLooping = False
+    else:
+        print("Invalid Response, type either Y/N")
+        keepLooping = True
